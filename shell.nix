cb83c0b07d4307728d7c5e37adae63a7aa1e278b
strinsberg <stevendkom@gmail.com>
2024-04-22 18:15:46 -0600

    Added c++ tools to nix shell

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
 pkgs.mkShell {
 
   nativeBuildInputs = with pkgs; [
-    cmake pkg-config gcc13 clang-tools_17
+    cmake pkg-config gcc13 clang-tools_17 gdb valgrind
   ];
 
   buildInputs = with pkgs; [
