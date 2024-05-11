429a7dbbad6711d67f8eaa52e1d398064fa22421
strinsberg <stevendkom@gmail.com>
2024-05-11 15:55:07 -0600

    Added tests and functionality for core/*.

--- src/core.cpp
+++ src/core.cpp
@@ -48,7 +48,39 @@ Value liscpp::__core__::sum(const Value args[], uint32_t n) {
 
 Value liscpp::__core__::sub(const Value[], uint32_t) { return Value(); }
 
-Value liscpp::__core__::product(const Value[], uint32_t) { return Value(); }
+Value liscpp::__core__::product(const Value args[], uint32_t n) {
+  int64_t x = 1;
+  double y = 1.0;
+  bool is_double = false;
+
+  for (uint32_t i = 0; i < n; ++i) {
+    switch (args[i].get_type()) {
+    case ValueType::Float:
+      if (!is_double) {
+        is_double = true;
+        y = double(x);
+      }
+      y = y * args[i].as_float();
+      break;
+    case ValueType::Int:
+      if (is_double) {
+        y = y * args[i].as_int();
+      } else {
+        x = x * args[i].as_int();
+      }
+      break;
+    default:
+      throw __error__::new_invalid_arg_error(
+          new GcString("core/*"), i, ValueType::Number, args[i].get_type());
+    }
+  }
+
+  if (is_double) {
+    return Value(y);
+  } else {
+    return Value(x);
+  }
+}
 
 Value liscpp::__core__::divide(const Value[], uint32_t) { return Value(); }
 
--- test/test_core.cpp
+++ test/test_core.cpp
@@ -7,6 +7,10 @@
 using namespace liscpp;
 using liscpp::__core__::call;
 
+// Arithmetic /////////////////////////////////////////////////////////////////
+
+// Sum //
+
 TEST(CoreLibArithmetic, sum_with_no_args) {
   const Value args[] = {};
   Value result = call(core::_plus_.as_fn(), args, 0);
@@ -41,11 +45,56 @@ TEST(CoreLibArithmetic, sum_with_many_args) {
                               Value(int64_t(4)), Value(0.5)};
   Value mixed_result = call(core::_plus_.as_fn(), mixed_args, 5);
   ASSERT_TRUE(mixed_result.is_float());
-  EXPECT_EQ(mixed_result.as_float(), 8.7);
+  EXPECT_FLOAT_EQ(mixed_result.as_float(), 8.7);
 }
 
 TEST(CoreLibArithmetic, sum_with_invalid_arg_type) {
   const Value args[] = {Value(int64_t(1)), Value()};
 
   EXPECT_THROW(call(core::_plus_.as_fn(), args, 2), Error *);
+}
+
+// Product //
+
+TEST(CoreLibArithmetic, product_with_no_args) {
+  const Value args[] = {};
+  Value result = call(core::_star_.as_fn(), args, 0);
+  EXPECT_EQ(result, Value(int64_t(1)));
+}
+
+TEST(CoreLibArithmetic, product_with_one_arg) {
+  const Value int_args[] = {Value(int64_t(42))};
+  Value int_result = call(core::_star_.as_fn(), int_args, 1);
+  EXPECT_EQ(int_result, Value(int64_t(42)));
+
+  const Value float_args[] = {Value(0.1234)};
+  Value float_result = call(core::_star_.as_fn(), float_args, 1);
+  ASSERT_TRUE(float_result.is_float());
+  EXPECT_FLOAT_EQ(float_result.as_float(), 0.1234);
+}
+
+TEST(CoreLibArithmetic, product_with_many_args) {
+  const Value int_args[] = {Value(int64_t(1)), Value(int64_t(2)),
+                            Value(int64_t(3)), Value(int64_t(4)),
+                            Value(int64_t(5))};
+  Value int_result = call(core::_star_.as_fn(), int_args, 5);
+  EXPECT_EQ(int_result, Value(int64_t(120)));
+
+  const Value float_args[] = {Value(0.1), Value(0.2), Value(0.3), Value(0.4),
+                              Value(0.5)};
+  Value float_result = call(core::_star_.as_fn(), float_args, 5);
+  ASSERT_TRUE(float_result.is_float());
+  EXPECT_FLOAT_EQ(float_result.as_float(), 0.0012);
+
+  const Value mixed_args[] = {Value(int64_t(1)), Value(0.2), Value(int64_t(3)),
+                              Value(int64_t(4)), Value(0.5)};
+  Value mixed_result = call(core::_star_.as_fn(), mixed_args, 5);
+  ASSERT_TRUE(mixed_result.is_float());
+  EXPECT_FLOAT_EQ(mixed_result.as_float(), 1.2);
+}
+
+TEST(CoreLibArithmetic, product_with_invalid_arg_type) {
+  const Value args[] = {Value(int64_t(1)), Value()};
+
+  EXPECT_THROW(call(core::_star_.as_fn(), args, 2), Error *);
 }
